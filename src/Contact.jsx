58f28739f8c39c89444d5e73b04939042b332ba3
oldmcdonnell <oldmcdonnell@gmail.com>
2024-04-28 16:29:36 -0400

    background

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,7 +6,6 @@ import github from '../img/Github.svg'
 function Contact() {
   return (
     <div className="p-5">
-        <MyNavbar />
         <div className="d-flex justify-content-center"><Link to='/'><img src={selfie} className="profileImage"></img></Link></div>
 
         <div className="d-flex justify-content-center"><h1>Contacts</h1></div>
