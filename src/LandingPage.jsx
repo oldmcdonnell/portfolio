1f7c40f7a71ed358d6003c61b574ecea7c7821c9
oldmcdonnell <oldmcdonnell@gmail.com>
2024-05-14 09:03:32 -0400

    updating blurb

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -20,8 +20,9 @@ function LandingPage (){
         <div className='d-flex mx-auto col-6'>
             <img className='paintingImage' src={Painting} />
             <p>
-                Some sample asdfasdfa asdf asdf asdf asdf asdf   
-                tesxt
+               Hello and welcome to my portfolio. I am a software developer who has a lot of experience in things that are not easy to show off. A lot of my work
+               has been with SQL databases. Helping troubleshoot problem for companies like Diebold, Garda, and Brinks.I have been interested and working in tech for many years.
+               I have had a lot of different roles like technical support, to quality assurance, to animator, to programmer.   
             </p>
 
         </div>
