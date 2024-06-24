a224b28f65d64c0e7a884c2d5497199ac85b357a
oldmcdonnell <oldmcdonnell@gmail.com>
2024-06-24 13:12:13 -0400

    correcting contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,51 +1,56 @@
-import { Link } from "react-router-dom"
-import selfie from '../img/selfie.jpg'
-import Email from '../img/Email.svg'
-import Github from '../img/Github.svg'
-import Instagram from '../img/Instagram.svg'
-import Phone from  '../img/Phone.svg'
-
+import { Link } from "react-router-dom";
+import selfie from '../img/selfie.jpg';
+import Email from '../img/Email.svg';
+import Github from '../img/Github.svg';
+import Instagram from '../img/Instagram.svg';
+import Phone from '../img/Phone.svg';
 
 function Contact() {
   return (
     <>
-    <div className="p-5">
-        <div className="d-flex justify-content-center"><Link to='/'><img src={selfie} className="profileImage"></img></Link></div>
+      <div className="p-5 text-center">
+        <Link to='/'>
+          <img src={selfie} alt="Profile" className="profileImage"/>
+        </Link>
+        <h1>Contact</h1>
+      </div>
 
-        <div className="d-flex justify-content-center"><h1>Contacts</h1></div>
-    </div>
-    <div className="container">
-    <div className="row justify-content-center align-items-md-center">
-      <div className="d-flex justify-content-center col-sm-6 col-md-4 p-3 bg-light semiOpacity">
-        <a href="mailto:oldmcdonnell@gmail.com">
-          <img src={Email} class="iconStyle" />
-        </a>
-        </div>
-      <div className="d-flex justify-content-center col-sm-6 col-md-4 p-3 bg-light semiOpacity">
-        <img src={Phone} class="iconStyle" /> </div>
-      <div className="d-flex justify-content-center col-sm-6 col-md-4 p-3 bg-light semiOpacity">
-        <a href="https://github.com/oldmcdonnell">
-          <img src={Github} class="iconStyle" />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-sm-6 col-md-4 p-3 bg-light semiOpacity text-center">
+            <a href="mailto:oldmcdonnell@gmail.com">
+              <img src={Email} alt="Email" className="iconStyle" />
             </a>
-            </div>
-    </div>
-    <div className="row">
-      <div className="d-flex justify-content-center col-sm-6 col-md-4 p-3 bg-light semiOpacity">
-        <a href="gram.com/mcdonnell.eoin/">
-          <img src={Instagram} class="iconStyle" />
-            </a></div>
-      <div className="d-flex justify-content-center col-sm-6 col-md-4 p-3 bg-light semiOpacity">
-        <a href="https://www.linkedin.com/in/eoin-mcdonnell-01078923/">
-          <i class="fa-brands fa-linkedin awesomeIcon">
-            </i></a></div>
-      <div className="d-flex justify-content-center col-sm-6 col-md-4 p-3 bg-light semiOpacity">
-        <a href="https://www.deviantart.com/aliramojo">
-          <i class="fa-brands fa-deviantart awesomeIcon">
-            </i></a></div>
-    </div>
-  </div>
-  </>
-  )
+          </div>
+          <div className="col-sm-6 col-md-4 p-3 bg-light semiOpacity text-center">
+            <img src={Phone} alt="Phone" className="iconStyle" />
+          </div>
+          <div className="col-sm-6 col-md-4 p-3 bg-light semiOpacity text-center">
+            <a href="https://github.com/oldmcdonnell">
+              <img src={Github} alt="GitHub" className="iconStyle" />
+            </a>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-sm-6 col-md-4 p-3 bg-light semiOpacity text-center">
+            <a href="https://instagram.com/mcdonnell.eoin/">
+              <img src={Instagram} alt="Instagram" className="iconStyle" />
+            </a>
+          </div>
+          <div className="col-sm-6 col-md-4 p-3 bg-light semiOpacity text-center">
+            <a href="https://www.linkedin.com/in/mcdonnell-eoin/">
+              <i className="fa-brands fa-linkedin awesomeIcon"></i>
+            </a>
+          </div>
+          <div className="col-sm-6 col-md-4 p-3 bg-light semiOpacity text-center">
+            <a href="https://www.deviantart.com/aliramojo">
+              <i className="fa-brands fa-deviantart awesomeIcon"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
