3808b7c0ecddc812c6fe0774c09c93183334766a
oldmcdonnell <oldmcdonnell@gmail.com>
2024-06-24 14:01:04 -0400

    updated blog CSS

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -13,34 +13,68 @@ function Blog() {
     };
 
     return (
-        <Container>
+        <Container id="page-content">
             <div>
                 <div className="my-4 text-center">
                     <h2>Blog</h2>
                 </div>
 
                 <div>
-                    <h4 onClick={() => toggleExpand('Week6')} style={{ cursor: 'pointer' }}>
+                    <h4
+                        onClick={() => toggleExpand('Week6')}
+                        className="blog-header"
+                    >
                         Week 6 {expandedPost === 'Week6' ? '-' : '+'}
                     </h4>
-                    {expandedPost === 'Week6' ? <Week6 /> : <p>Preview of Week 6...</p>}
+                    {expandedPost === 'Week6' ? (
+                        <Week6 />
+                    ) : (
+                        <p className="blog-preview">
+                            Preview of Week 6... [Replace with actual preview text]
+                        </p>
+                    )}
 
-
-                    <h4 onClick={() => toggleExpand('Week7')} style={{ cursor: 'pointer' }}>
+                    <h4
+                        onClick={() => toggleExpand('Week7')}
+                        className="blog-header"
+                    >
                         Week 7 {expandedPost === 'Week7' ? '-' : '+'}
                     </h4>
-                    {expandedPost === 'Week7' ? <Week7 /> : <p>Preview of Week 7...</p>}
+                    {expandedPost === 'Week7' ? (
+                        <Week7 />
+                    ) : (
+                        <p className="blog-preview">
+                            Preview of Week 7... [Replace with actual preview text]
+                        </p>
+                    )}
 
-
-                    <h4 onClick={() => toggleExpand('Week8')} style={{ cursor: 'pointer' }}>
+                    <h4
+                        onClick={() => toggleExpand('Week8')}
+                        className="blog-header"
+                    >
                         Week 8 {expandedPost === 'Week8' ? '-' : '+'}
                     </h4>
-                    {expandedPost === 'Week8' ? <Week8 /> : <p>Preview of Week 8...</p>}
+                    {expandedPost === 'Week8' ? (
+                        <Week8 />
+                    ) : (
+                        <p className="blog-preview">
+                            Preview of Week 8... [Replace with actual preview text]
+                        </p>
+                    )}
 
-                    <h4 onClick={() => toggleExpand('Week9')} style={{ cursor: 'pointer' }}>
+                    <h4
+                        onClick={() => toggleExpand('Week9')}
+                        className="blog-header"
+                    >
                         Week 9 {expandedPost === 'Week9' ? '-' : '+'}
                     </h4>
-                    {expandedPost === 'Week9' ? <Week9 /> : <p>Preview of Week 9...</p>}
+                    {expandedPost === 'Week9' ? (
+                        <Week9 />
+                    ) : (
+                        <p className="blog-preview">
+                            Preview of Week 9... [Replace with actual preview text]
+                        </p>
+                    )}
                 </div>
             </div>
         </Container>
