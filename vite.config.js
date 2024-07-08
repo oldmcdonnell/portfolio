4dc74fb8036ccf594d4c2ed805ef2a5dac5b853a
oldmcdonnell <oldmcdonnell@gmail.com>
2024-07-08 17:16:53 -0400

    going back to old

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+      
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8080,
-    hot: true
-  },
-  build: {
-    outDir: 'docs',
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    }
+export default defineConfig(({}) => {
+  return {
+    base: '/portfolio/',
+    plugins: [react()],
+    server: {
+      port: 8080,
+      hot: true
+    },
   }
 })
