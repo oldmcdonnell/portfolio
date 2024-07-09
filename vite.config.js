45ad2bdb78b8cb5c392c3469f8abb046eb32ecde
oldmcdonnell <oldmcdonnell@gmail.com>
2024-07-09 14:46:32 -0400

    updating config file

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-      
+
 // https://vitejs.dev/config/
-export default defineConfig(({}) => {
-  return {
-    base: '/portfolio/',
-    plugins: [react()],
-    server: {
-      port: 8080,
-      hot: true
-    },
+export default defineConfig({
+  base: '/portfolio/',
+  plugins: [react()],
+  server: {
+    port: 8080,
+    hot: true
+  },
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   }
 })
