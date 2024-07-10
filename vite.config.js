9dc0e43aa0c247b8196af589303109d5c02327de
oldmcdonnell <oldmcdonnell@gmail.com>
2024-07-10 14:46:49 -0400

    this works locally, the other one did not

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-      
+
 // https://vitejs.dev/config/
-export default defineConfig(({}) => {
-  return {
-    plugins: [react()],
-    server: {
-      port: 8080,
-      hot: true
-    },
+export default defineConfig({
+  base: '/portfolio/',
+  plugins: [react()],
+  server: {
+    port: 8080,
+    hot: true
+  },
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   }
 })
+// should this be just vite.config?
