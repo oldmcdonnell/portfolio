01f88cd7bc59541993762627da9379197f74a258
oldmcdonnell <oldmcdonnell@gmail.com>
2024-07-31 22:43:03 -0400

    fixing contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -15,29 +15,29 @@ function Contact() {
       <h1 className="text-light">Contact</h1>
 
       <Row className="justify-content-center mt-4">
-        <Col xs={6} sm={4} className="p-2 icon-container">
+        <Col xs={6} md={4} className="p-2 icon-container">
           <a href="mailto:oldmcdonnell@gmail.com">
             <img src={Email} alt="Email" className="iconStyle" />
           </a>
         </Col>
-        <Col xs={6} sm={4} className="p-2 icon-container">
+        <Col xs={6} md={4} className="p-2 icon-container">
           <a href="https://www.linkedin.com/in/mcdonnell-eoin/">
             <i className="fa-brands fa-linkedin awesomeIcon"></i>
           </a>
         </Col>
-        <Col xs={6} sm={4} className="p-2 icon-container">
+        <Col xs={6} md={4} className="p-2 icon-container">
           <a href="https://github.com/oldmcdonnell">
             <img src={Github} alt="GitHub" className="iconStyle" />
           </a>
         </Col>
       </Row>
       <Row className="justify-content-center mt-4">
-        <Col xs={6} sm={4} className="p-2 icon-container">
+        <Col xs={6} md={4} className="p-2 icon-container">
           <a href="https://instagram.com/mcdonnell.eoin/">
             <img src={Instagram} alt="Instagram" className="iconStyle" />
           </a>
         </Col>
-        <Col xs={6} sm={4} className="p-2 icon-container">
+        <Col xs={6} md={4} className="p-2 icon-container">
           <a href="https://www.deviantart.com/aliramojo">
             <i className="fa-brands fa-deviantart awesomeIcon"></i>
           </a>
